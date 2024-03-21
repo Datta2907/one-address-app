a4a1599794d5507bf24d60dde5c01b35d2dfbbbe
Datta2907 <datta598764@gmail.com>
2024-03-21 23:00:18 +0530

    validation in backend as js can be disabled by client

--- controllers/authentication/auth.js
+++ controllers/authentication/auth.js
@@ -25,11 +25,17 @@ exports.register = async (req, res) => {
     try {
         const user = await User.findOne({ email: req.params.email });
         if (user) {
-            return res.status(200).json({ success: false, message: 'Email Registered, would you like to login?' });
+            return res.status(200).json({ success: true, message: 'Email Registered, would you like to login?' });
         } else {
             const { firstName, lastName, password, role, email } = req.body;
-            if (!firstName || !lastName || !password || !role) {
-                return res.status(200).json({ success: false, message: 'Please Provide all the details!' });
+            if (!firstName || !lastName || !password || !role || !email) {
+                return res.status(412).json({ success: false, message: 'Please Provide all the details!' });
+            }
+            if (!/^[a-z]+$/i.test(firstName) || !/^[a-z]+$/i.test(lastName)) {
+                return res.status(412).json({ success: false, message: 'Names can have only letters!' });
+            }
+            if (!email.match(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)) {
+                return res.status(412).json({ success: false, message: 'Invalid Email!' });
             }
             let salt = await bcrypt.genSalt(Number(process.env.SALT_ROUNDS));
             const encryptedPassword = await bcrypt.hash(password, salt);
